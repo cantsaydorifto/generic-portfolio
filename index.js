9a867c16217549ff995f70038b284d61b402932d
cantsaydorifto <108617883+cantsaydorifto@users.noreply.github.com>
2024-08-27 13:26:00 +0530

    fix modal

--- index.js
+++ index.js
@@ -8,11 +8,13 @@ triggerModalButton.addEventListener("click", () => {
   modalOverlay.style.display = "flex";
 });
 
-cancelButton.addEventListener("click", () => {
+cancelButton.addEventListener("click", (event) => {
+  event.preventDefault();
   modalOverlay.style.display = "none";
 });
 
 window.addEventListener("click", (event) => {
+  event.preventDefault();
   if (event.target === modalOverlay) {
     modalOverlay.style.display = "none";
   }
